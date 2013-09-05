c6bbdaa06df1cf545c402d3f42444c81fdde4353
llj <lingjun.li1@gmail.com>
2013-09-05 16:56:50 +0800

    added key gen test with sjcl

--- media/js/file_decrypt.js
+++ media/js/file_decrypt.js
@@ -1,18 +1,24 @@
 importScripts('CryptoJS/rollups/aes.js');
 importScripts('CryptoJS/rollups/pbkdf2.js');
-
+importScripts('sjcl.min.js');
 self.onmessage = function (oEvent) {
     decrypt(oEvent);
 }
 
 function decrypt(oEvent) {
   var decrypted = {index: oEvent.data.index};
-  var salt = CryptoJS.lib.WordArray.random(128/8);
+  var salt = CryptoJS.lib.WordArray.create(salt2);
   var t_start = new Date().getTime();
   // key gen
   var key = CryptoJS.PBKDF2("Secret Passphrase", salt, { keySize: 256/32, iterations: 1000 });
 
   decrypted.key_gen_time = new Date().getTime() - t_start;
+
+  var salt2 = [0xda, 0x90, 0x45, 0xc3, 0x06, 0xc7, 0xcc, 0x26];
+  var t_start2 = new Date().getTime();
+  var key2 = sjcl.misc.pbkdf2("Secret Passphrase", salt2, 1000, 256, null);
+  decrypted.key_gen_time2 = new Date().getTime() - t_start2;
+
   var fileData = oEvent.data.block;
   var passphrase = oEvent.data.passphrase;
   try{
